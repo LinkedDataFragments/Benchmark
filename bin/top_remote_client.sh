969253adcd6484bd448d0f542d23d841126671ce
Ruben Taelman <ruben.taelman@ugent.be>
2016-04-01 11:16:46 +0200

    Fix remote measurements on Amazon EC2

--- bin/top_remote_client.sh
+++ bin/top_remote_client.sh
@@ -15,7 +15,7 @@ localid=$4
 pid=$5
 
 # -oStrictHostKeyChecking=no   This always accepts server fingerprint
-./retrying-sshpass.sh -p "$password" ssh -t -t -oStrictHostKeyChecking=no $username@$location <<ENDSSH | ./filteruntil.sh "---START---" 2
+./retrying-sshpass.sh -p "$password" ssh -t -t -oStrictHostKeyChecking=no $username@$location <<ENDSSH | ./filteruntil.sh "---START---" 3
 sudo docker exec -i client-$localid /bin/bash <<ENDDOCKER
 echo "---START---"
 top -p $pid -b -d 1 | gawk '{if(\\\$1 == '$pid'){ print \\\$1 " " \\\$9 " " \\\$6 "K ";system(""); }}'
